8435a954b62b44531ec00ce8b91a71062ad2d109
teste2811 <armarianam12@gmail.com>
2025-02-14 14:56:37 -0300

    componentes single item e item list

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SingleItem from './SingleItem'
+
+
+const ItemList = ({title, items}) => {
+
+  return (
+    <div className='item-list'>
+                <div className='item-list__header'>
+                    <h2>{title} populares</h2>
+                    <a href="" className='item-list__link'>Mostrar tudo</a>
+                </div>
+    
+                <div className="item-list__container">
+                    {Array(items).fill().map((currentValue, index)=> <SingleItem key={`${title}-${index}`}/>)}
+                </div>
+    
+            </div>
+  )
+}
+
+export default ItemList
